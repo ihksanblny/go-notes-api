b067f0f6b3f3560492a2b346f6ea5931b03a5004
ihksanblny <isanomuzumi878@gmail.com>
2025-11-25 12:36:01 +0700

    setup : setup GO

---
+++ go-notes-api/cmd/api/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	
+
+)
+
+func main() {
+	// in memory store for testing
+	store := notes.NewInMemoryStore()
+	notes.SeedInitialNotes(store)
+
+	handler := notes.NewHandler(store)
+
+	mux := http.NewServeMux()
+	mux.Handle("/notes/", http.HandlerFunc(handler.HandleNotes)) // Handle /notes/ endpoint
+	mux.Handle("/notes/", http.HandlerFunc(handler.HandleNotesByID)) // Handle /notes/{id} endpoint
+
+	handlerWithMiddleware := server.WithCors(mux, "http://localhost:5173")
+
+	addr := ":8080"
+	log.Println("Starting server on", addr)
+
+	if err := http.ListenAndServe(addr, handlerWithMiddleware); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
---
+++ go-notes-api/go.mod
@@ -0,0 +1,3 @@
+module go-notes-api
+
+go 1.25.3
